af7d52c3803890c32896deb6bcb0f232acc9494b
夏磊 <xialei@xialeiMacBook-Pro.local>
2017-03-03 11:10:05 +0800

    update

--- mybatis/src/main/java/org/adrian/mybatistest/controller/UserController.java
+++ mybatis/src/main/java/org/adrian/mybatistest/controller/UserController.java
@@ -3,6 +3,7 @@ package org.adrian.mybatistest.controller;
 import org.adrian.mybatistest.domain.ResultDO;
 import org.adrian.mybatistest.domain.UserDO;
 import org.adrian.mybatistest.service.UserService;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,11 @@ public class UserController {
 
     @RequestMapping(value = "createOne", method = RequestMethod.POST)
     public ResultDO createOne(@RequestBody UserDO user) {
+        DateTime createTime = DateTime.now();
+        user.setCreateTime(createTime);
+        user.setUpdateTime(createTime);
+        user.setCreateBy(0L);
+        user.setUpdateBy(0L);
         Long data = userService.createOne(user);
         ResultDO resultDO = new ResultDO();
         resultDO.setResultCode(0);
