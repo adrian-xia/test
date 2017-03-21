c2a6a9479d18a000d52d43f5b73722eab8afacc3
夏磊 <xialei@xialeiMacBook-Pro.local>
2017-03-21 15:45:46 +0800

    update

--- jvm/src/main/java/jvm/chapter02/oom/HeapOOM.java
+++ jvm/src/main/java/jvm/chapter02/oom/HeapOOM.java
@@ -5,17 +5,24 @@ import java.util.List;
 
 /**
  * JAVA堆溢出
- * Created by xialei on 2017/3/17.
+ *
+ -Xms20M        堆最小
+ -Xmx20M        堆最大
+ -Xmn10M        堆年轻代大小
+ -XX:+PrintGCDetails    打印GC详细信息
+ -XX:SurvivorRatio=8    Eden区与Survivor区的大小比值
+ -XX:+HeapDumpOnOutOfMemoryError    当堆出现OOM时, 输出dump文件
  */
 public class HeapOOM {
 
-    static class OOMObject {}
+    private static class OOMObject {}
 
     public static void main(String[] args) {
         List<OOMObject> list = new ArrayList<>();
 
         while (true) {
             list.add(new OOMObject());
+//            System.out.println(list.size());
         }
     }
 }
