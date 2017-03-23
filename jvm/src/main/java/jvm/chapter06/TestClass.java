b89bba8bb2fedba6f19d55f16ea58cd7be97ebeb
夏磊 <xialei@192.168.0.102>
2017-03-23 10:04:00 +0800

    update

---
+++ jvm/src/main/java/jvm/chapter06/TestClass.java
@@ -0,0 +1,14 @@
+package jvm.chapter06;
+
+/**
+ * Created by xialei on 2017/3/22.
+ */
+public class TestClass {
+
+
+    private int m;
+    public int inc() {
+        return m + 1;
+    }
+
+}
---
+++ jvm/src/main/java/jvm/chapter06/TestGouzao.java
@@ -0,0 +1,28 @@
+package jvm.chapter06;
+
+/**
+ * Created by xialei on 2017/3/23.
+ */
+public class TestGouzao {
+
+    int i;
+    int j;
+
+    public TestGouzao() {
+        i = 1;
+    }
+
+    {
+        j = 2;
+    }
+
+
+    public void test(String a, int b){
+
+    }
+
+    public void test(int a, String b) {
+
+    }
+
+}
