7a83742109ccb8e97d78f5a6133fa47e26c107ad
夏磊 <xialei@xialeiMacBook-Pro.local>
2017-03-24 12:59:56 +0800

    update

---
+++ jvm/src/main/java/jvm/chapter06/TestDiv.java
@@ -0,0 +1,22 @@
+package jvm.chapter06;
+
+/**
+ * Created by xialei on 2017/3/24.
+ */
+public class TestDiv {
+
+    /**
+     * 将会输出Infinity
+     * 虚拟机对于浮点数的运算，不会抛出异常，但是会出现一个未知的符号
+     * 虚拟机中的double0.0与int0的存储方式不同，所以不会抛出异常。
+     * 当 0.0 / 0.0 时，输出NaN(非数)
+     * 当 1.0 / 0.0 时，输出Infinity(无穷大)
+     *
+     * @param args
+     */
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        System.out.println("1.0 / 0.0 = " + 1.0 / 0.0);
+        System.out.println("0.0 / 0.0 = " + 0.0 / 0.0);
+    }
+}
