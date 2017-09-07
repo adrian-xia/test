e040005bb299f0ac33529af35c04ff0101023e51
xialei <xialei@houbank.cn>
2017-09-07 12:38:23 +0800

    update

--- redis/src/main/java/org/adrian/test/redis/redisinaction/chapter01/TestVote.java
+++ redis/src/main/java/org/adrian/test/redis/redisinaction/chapter01/TestVote.java
@@ -26,7 +26,7 @@ public class TestVote {
 
     @Test
     public void testPostArticle() {
-        System.out.println(postArticle(jedis, "xialei", "redis_in_action", "redis.io"));
+        System.out.println(postArticle(jedis, "xugang", "spring_in_action", "spring.io"));
     }
 
     private String postArticle(Jedis conn, String user, String title, String link) {
@@ -107,10 +107,23 @@ public class TestVote {
         System.out.println(getGroupArticles(jedis, "facebook", 1, "score:"));
     }
 
+    /**
+     * ZUNIONSTORE destination numkeys key [key ...] [WEIGHTS weight [weight ...]] [AGGREGATE SUM|MIN|MAX]
+     * 计算给定的一个或多个有序集的并集，其中给定 key 的数量必须以 numkeys 参数指定，并将该并集(结果集)储存到 destination 。
+     * 默认情况下，结果集中某个成员的 score 值是所有给定集下该成员 score 值之 和 。
+     * WEIGHTS
+     * 使用 WEIGHTS 选项，你可以为 每个 给定有序集 分别 指定一个乘法因子(multiplication factor)，
+     * 每个给定有序集的所有成员的 score 值在传递给聚合函数(aggregation function)之前都要先乘以该有序集的因子。
+     * 如果没有指定 WEIGHTS 选项，乘法因子默认设置为 1 。
+     * AGGREGATE
+     * 使用 AGGREGATE 选项，你可以指定并集的结果集的聚合方式。
+     * 默认使用的参数 SUM ，可以将所有集合中某个成员的 score 值之 和 作为结果集中该成员的 score 值；使用参数 MIN ，
+     * 可以将所有集合中某个成员的最小score值作为结果集中该成员的score值；而参数MAX则是将所有集合中某个成员的最大score值作为结果集中该成员的score值。
+     */
     private List<Map<String, String>> getGroupArticles(Jedis conn, String group, int page, String order) {
         String key = order + group;//检查是否有已缓存的排序结果
         if (!conn.exists(key)) {
-            ZParams params = new ZParams().aggregate(ZParams.Aggregate.MAX);//todo
+            ZParams params = new ZParams().aggregate(ZParams.Aggregate.MIN);//todo
             conn.zinterstore(key, params, "group:" + group, order);
             conn.expire(key, 60);//缓存60秒
         }
